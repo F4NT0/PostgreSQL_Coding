06637f4a6b5c8e6d4eb146e4cab4d03833e27158
fanto <gabriel.fanto@digifred.com.br>
2021-03-02 15:50:21 -0300

    Inicio de Procedure

---
+++ Procedures/base_procedure.sql
@@ -0,0 +1,13 @@
+create function nome_da_funcao() returns trigger
+  language plpgsql
+as
+$$
+declare
+  -- aqui se declara as variaveis
+begin
+  -- aqui se coloca as funcionalidades
+end
+$$;
+
+-- definir como owner o postgres
+alter function nome_da_funcao() owner to postgres;
